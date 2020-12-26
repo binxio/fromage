950a199fc518524df885f6b3b88cb1c0aa19db66
Mark van Holsteijn <mark.van.holsteijn@gmail.com>
2020-12-27 00:38:41 +0100

    added tag exploration to detected outdated versions

--- go.mod
+++ go.mod
@@ -3,7 +3,9 @@ module github.com/binxio/git-fromage
 go 1.15
 
 require (
+	github.com/docker/distribution v2.7.1+incompatible
 	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+	github.com/google/go-containerregistry v0.3.0
 	github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd
 	github.com/mitchellh/go-homedir v1.1.0
 	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
---
+++ tag.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/go-containerregistry/pkg/crane"
+	"github.com/google/go-containerregistry/pkg/name"
+	"regexp"
+	"sort"
+	"strconv"
+)
+
+type Tag struct {
+	Literal  string
+	Prefix   string
+	Suffix   string
+	Version  []int
+	Category string
+}
+
+type Tags []Tag
+
+var (
+	semVerRegExp      = regexp.MustCompile(`(?m)^(?P<prefix>[^0-9]*)(?P<major>[0-9]+)((\.(?P<minor>[0-9]+)?(\.(?P<patch>[0-9]+))?(?P<suffix>.*))|(?P<suffix2>\W.*))$`)
+	semVerRegExpNames = semVerRegExp.SubexpNames()
+)
+
+func MakeTag(tag string) Tag {
+	var result = Tag{
+		Literal: tag,
+		Version: make([]int, 0, 3),
+	}
+
+	matches := semVerRegExp.FindStringSubmatch(tag)
+	if matches != nil {
+		for i, name := range semVerRegExpNames {
+			switch name {
+			case "prefix":
+				result.Prefix = matches[i]
+			case "suffix", "suffix2":
+				if len(matches[i]) > 0 {
+					result.Suffix = matches[i]
+				}
+			case "major", "minor", "patch":
+				if len(matches[i]) > 0 {
+					level, _ := strconv.Atoi(matches[i])
+					result.Version = append(result.Version, level)
+				}
+			default:
+				// ignore
+			}
+		}
+	} else {
+		result.Prefix = tag
+	}
+	if result.Prefix != "" || result.Suffix != "" {
+		result.Category = fmt.Sprintf("%s|%s", result.Prefix, result.Suffix)
+	}
+
+	return result
+}
+
+func compareInt(a int, b int) int {
+	if a < b {
+		return -1
+	}
+	if a > b {
+		return 1
+	}
+	return 0
+}
+func compareVersion(a []int, b []int) int {
+	var result int
+	for i, v := range a {
+		if i < len(b) {
+			result = compareInt(v, b[i])
+			if result != 0 {
+				return result
+			}
+		} else {
+			return 1
+		}
+	}
+	if len(a) < len(b) {
+		return -1
+	}
+	return 0
+}
+
+func (a Tag) Compare(b Tag) int {
+	return compareVersion(a.Version, b.Version)
+}
+
+func (a Tag) Equals(b Tag) bool {
+	return a.Literal == b.Literal &&
+		a.Prefix == b.Prefix &&
+		a.Suffix == b.Suffix &&
+		a.Category == b.Category &&
+		compareVersion(a.Version, b.Version) == 0
+}
+
+func ListAllTags(reference name.Reference) ([]Tag, error) {
+
+	tags, err := crane.ListTags(reference.Context().String())
+	if err != nil {
+		return nil, fmt.Errorf("could not retrieve tags for %s", reference)
+	}
+
+	result := make([]Tag, len(tags), len(tags))
+	for i, tag := range tags {
+		result[i] = MakeTag(tag)
+	}
+
+	return result, nil
+}
+
+type TagList []Tag
+type TagCategories map[string]TagList
+
+func (l TagList) FindGreaterThan(tag Tag) TagList {
+	result := make(TagList, 0)
+	for _, t := range l {
+		if len(t.Version) == len(tag.Version) && tag.Compare(t) < 0 {
+			result = append(result, t)
+		}
+	}
+	return result
+}
+
+func (a TagList) Len() int           { return len(a) }
+func (a TagList) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a TagList) Less(i, j int) bool { return (a[i]).Compare(a[j]) < 0 }
+
+func MakeTagCategories(tags TagList) TagCategories {
+	var result = make(map[string]TagList)
+	for _, tag := range tags {
+		if list, ok := result[tag.Category]; ok {
+			result[tag.Category] = append(list, tag)
+		} else {
+			result[tag.Category] = []Tag{tag}
+		}
+	}
+	for _, tags := range result {
+		sort.Sort(tags)
+	}
+	return result
+}
